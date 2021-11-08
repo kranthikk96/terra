b4070793302f8fde938c7e90a022c91f1567297b
kranthikk96 <87638903+kranthikk96@users.noreply.github.com>
2021-11-08 13:37:54 +0530

    Update terraform main.tf

--- terraform main.tf
+++ terraform main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
+    access_key = "${var.AWS_ACCESS_ACCESS_KEY}"
+    secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
     region = "${var.aws_region}"
 }
 
